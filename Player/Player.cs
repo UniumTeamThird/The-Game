fc6f0e1702dcdd19feb121f14dc4623b87238b42
Artemii <47931983+Jrol123@users.noreply.github.com>
2019-11-29 18:39:11 +1000

    H A S H

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +23,21 @@ namespace Player
             password = "";
             ID = 1 + players.Count();
             players.Add(ID);
+            FileStream add = new FileStream("C:/THE-GAME/USERLIST.txt", FileMode.Open);
+            StreamWriter addon = new StreamWriter(add);
+            add.Seek(0,SeekOrigin.End);
+            addon.WriteLine(ID);
+            addon.WriteLine(login);
+            byte[] tmpSource;
+            byte[] tmpHash;
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(password);
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            string pps = "";
+            foreach(int i in tmpHash)
+            {
+                pps += i;
+            }
+            addon.WriteLine(pps);
         }
         public void AddFriend(int id)
         {
