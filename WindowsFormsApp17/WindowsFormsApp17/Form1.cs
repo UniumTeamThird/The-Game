ff1206159734bf35bc134bf53fd8e5cbd8cdd153
izongol <57742529+izongol@users.noreply.github.com>
2019-12-26 15:30:14 +1000

    Проработка френдлиста.
    Добавлена функция добавки в друзья.

--- WindowsFormsApp17/WindowsFormsApp17/Form1.cs
+++ WindowsFormsApp17/WindowsFormsApp17/Form1.cs
@@ -13,31 +13,36 @@ namespace WindowsFormsApp17
 {
     public partial class Form1 : Form
     {
-
+        
         public Form1()
         {
             InitializeComponent();
-        }
+        }            
         private void button2_Click(object sender, EventArgs e)
         {
-            tabControl6.SelectedIndex = 1;
-
-        }
+            tabControl1.SelectedIndex = 1;
+            
+        }        
         private void button3_Click(object sender, EventArgs e)
         {
-            FileStream olo = new FileStream("C:/Users/Ученик/Desktop/Новая папка (4)/putis.txt", FileMode.OpenOrCreate);
+            FileStream olo = new FileStream("C:/Users/Ученик/Desktop/Новая папка (4)/login and pasvord", FileMode.OpenOrCreate);
             StreamWriter writer = new StreamWriter(olo);
-            olo.Seek(0, SeekOrigin.End);
+            FileStream popo = new FileStream("C:/Users/Ученик/Desktop/Новая папка (4)/frendlist", FileMode.OpenOrCreate);
+            StreamWriter writ = new StreamWriter(popo);
+            olo.Seek(0,SeekOrigin.End);
             writer.WriteLine(textBox3.Text);
+            writ.WriteLine(textBox3.Text);
             writer.WriteLine(textBox4.Text);
             writer.Close();
             olo.Close();
-
+            writ.Close();
+            popo.Close();
+            MessageBox.Show("Поздравляю с регистрацией");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            tabControl6.SelectedIndex = 0;
+            tabControl1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -46,83 +51,76 @@ namespace WindowsFormsApp17
             StreamReader reader = new StreamReader(olo);
             while (!reader.EndOfStream)
             {
-
+                
                 string p = reader.ReadLine();
+                int o = 0;
                 if (p == textBox1.Text)
                 {
-                    p = reader.ReadLine();
+                   o = 1;
+                   p = reader.ReadLine();
+                    
                     if (p == textBox2.Text)
                     {
+                        
                         MessageBox.Show("вы вошли");
-                        tabControl6.SelectedIndex = 2;
-                        break;
+                        tabControl1.SelectedIndex = 2;                    
+                        break;                        
+                        
                     }
-                    else
+                    else 
                     {
                         MessageBox.Show("пароль не верный");
                     }
+                    
+                    
+                } 
+                if (o!= 1)
+                {
+                    MessageBox.Show("пользователь не найден");
+                    break;
                 }
             }
             reader.Close();
             olo.Close();
 
-        }
+        }       
         int player1 = 0;
         int player2 = 0;
-        private void pictureBox1_Click_1(object sender, EventArgs e)
-        {
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
             if (player1 > 0)
             {
                 player2 = 1;
-                pictureBox1.Image = Properties.Resources.images6;
-                pictureBox1.Enabled = false;
-                pictureBox2.Enabled = false;
-                pictureBox3.Enabled = false;
             }
             else
             {
                 player1 = 1;
-                pictureBox4.Image = Properties.Resources.images10;
-                pictureBox1.Image = Properties.Resources.images6;
             }
         }
 
-        private void pictureBox2_Click_1(object sender, EventArgs e)
+        private void pictureBox2_Click(object sender, EventArgs e)
         {
             if (player1 > 0)
             {
                 player2 = 2;
-                pictureBox2.Image = Properties.Resources.images5;
-                pictureBox1.Enabled = false;
-                pictureBox2.Enabled = false;
-                pictureBox3.Enabled = false;
             }
             else
             {
                 player1 = 2;
-                pictureBox4.Image = Properties.Resources.images10;
-                pictureBox2.Image = Properties.Resources.images5;
             }
         }
 
-        private void pictureBox3_Click_1(object sender, EventArgs e)
+        private void pictureBox3_Click(object sender, EventArgs e)
         {
             if (player1 > 0)
             {
                 player2 = 3;
-                pictureBox3.Image = Properties.Resources.images4;
-                pictureBox1.Enabled = false;
-                pictureBox2.Enabled = false;
-                pictureBox3.Enabled = false;
             }
             else
             {
                 player1 = 3;
-                pictureBox4.Image = Properties.Resources.images10;
-                pictureBox3.Image = Properties.Resources.images4;
             }
-
         }
 
         private void button5_Click_1(object sender, EventArgs e)
@@ -132,132 +130,149 @@ namespace WindowsFormsApp17
                 MessageBox.Show("ПОБЕДИЛ 1 ИГРОК ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 1 && player2 == 3)
             {
                 MessageBox.Show("ПОБЕДИЛ 2 ИГРОК ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 1 && player2 == 1)
             {
                 MessageBox.Show("НИЧЬЯ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 2 && player2 == 1)
             {
                 MessageBox.Show("ПОБЕДИЛ 2 ИГРОК ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 2 && player2 == 2)
             {
                 MessageBox.Show("НИЧЬЯ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 2 && player2 == 3)
             {
                 MessageBox.Show("ПОБЕДИЛ 1 ИГРОК");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 3 && player2 == 3)
             {
                 MessageBox.Show("НИЧЬЯ");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 3 && player2 == 1)
             {
                 MessageBox.Show("ПОБЕДИЛ 1 ИГРОК");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
             }
             else if (player1 == 3 && player2 == 2)
             {
                 MessageBox.Show("ПОБЕДИЛ 2 ИГРОК");
                 player1 = 0;
                 player2 = 0;
-                pictureBox4.Image = Properties.Resources.images11;
-                pictureBox1.Image = Properties.Resources.images2;
-                pictureBox2.Image = Properties.Resources.images;
-                pictureBox3.Image = Properties.Resources.images3;
-                pictureBox1.Enabled = true;
-                pictureBox2.Enabled = true;
-                pictureBox3.Enabled = true;
+            }
+        }
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Close();
+        }    
+        private void button6_Click(object sender, EventArgs e)
+        {           
+
+            textBox6.Text = textBox6.Text + " "+ textBox5.Text;
+            textBox6.SelectionStart = textBox6.Text.Length;
+
+            textBox6.ScrollToCaret();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            if (player1 > 0)
+            {
+                player2 = 1;
+            }
+            else
+            {
+                player1 = 1;
             }
         }
 
-        private void button7_Click_1(object sender, EventArgs e)
+        private void pictureBox2_Click_1(object sender, EventArgs e)
         {
-            Close();
+            if (player1 > 0)
+            {
+                player2 = 2;
+            }
+            else
+            {
+                player1 = 2;
+            }
         }
-       
 
-        private void button6_Click_1(object sender, EventArgs e)
+        private void pictureBox3_Click_1(object sender, EventArgs e)
         {
-            textBox5.Text += textBox6.Text + Environment.NewLine;            
+            if (player1 > 0)
+            {
+                player2 = 3;
+            }
+            else
+            {
+                player1 = 3;
+            }
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Items.Add("qwerty");
+            comboBox2.Items.Add("qwerty1");
+            comboBox2.Items.Add("qwerty2");
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FileStream popo = new FileStream("C:/Users/Ученик/Desktop/Новая папка (4)/frendlist", FileMode.OpenOrCreate);
+            StreamReader reader = new StreamReader(popo);
+            while (!reader.EndOfStream)
+            {
+
+                string p = reader.ReadLine();
+                int o = 0;
+                if (p == textBox7.Text)
+                {
+                    o = 1;
+                    comboBox2.Items.Add(textBox7.Text);
+                }
+                if (o != 1)
+                {
+                    MessageBox.Show("пользователь не найден");
+                    break;
+                }
+            }
+            reader.Close();
+            popo.Close();
+        }
+
+        
     }
 }
-    
 
                                                                                                                                                                                                             
