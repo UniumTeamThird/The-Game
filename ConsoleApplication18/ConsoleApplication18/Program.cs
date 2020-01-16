fa5c48e401719a5d37f07645030239dbfb8f6a83
Artemii <47931983+Jrol123@users.noreply.github.com>
2020-01-16 17:18:16 +1000

    CHECK
    itt should work

---
+++ ConsoleApplication18/ConsoleApplication18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static string Check(int mId)
+        {
+            List<int> friends = Player[mId].FrRet;
+            string friendsOut = "";
+            for(int i  = 0; i < friends.Count; i ++)
+            {
+                if(i > 0)
+                {
+                    friendsOut += "_";
+                }
+                if(Player[friends[i]].IsOnline)
+                {
+                    friendsOut += friends[i];
+                }
+            }
+            return (friendsOut);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
