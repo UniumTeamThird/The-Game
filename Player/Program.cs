22586ea2ce1fd0c7caebf0801344c97b662b5040
Artemii <47931983+Jrol123@users.noreply.github.com>
2020-02-13 17:28:26 +1000

    Seems like i've addedan function

--- Player/Program.cs
+++ Player/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,45 @@ namespace Player
 {
     class Program
     {
+        static string WhoOnline(int persId, List<Player> alpha)
+        {
+            List<int> friends = alpha[persId].returnFriends();
+            string friendsOut = "OK";
+            for (int i = 0; i < friends.Count; i++)
+            {
+                friendsOut += "+";
+                if (alpha[friends[i]].IsOnline())
+                {
+                    friendsOut += friends[i];
+                }
+            }
+            return (friendsOut);
+
+        }//функция проверки друзей на онлайн
+        private const string host = "192.168.0.127";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+        static string Speach(string message)
+        {
+            client = new TcpClient();
+            client.Connect(host,port);
+            stream = client.GetStream();
+            BinaryReader receiver = new BinaryReader(stream);
+            BinaryWriter sender = new BinaryWriter(stream);
+            sender.Write(message);
+            sender.Flush();
+            return receiver.ReadString();
+        }
         static void Main(string[] args)
         {
-            new Player();
+            List <Player> alpha = new List<Player>();
+            alpha.Add(new Player());
+            int a = 0;
+            string allo = "";
+            allo = (WhoOnline(a, alpha));
+            Console.WriteLine(Speach(allo));
+            Console.ReadKey();
         }
     }
 }
